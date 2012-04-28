c629ac84211087879401eb791f534a79f6409f5e
Tiago de Campos <tiagocampo@gmail.com>
2012-04-28 18:05:30 -0300

    usefull changes

--- zepomo-indicator.sh
+++ zepomo-indicator.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
-function progress_bar {
+clear
 
+function trans_sec {
+   local D1=$1
+   local D2=$2
+   ((diff_sec=D1-D2))
+   echo - | awk '{printf "%d:%d","'"$diff_sec"'"%(60*60)/60,"'"$diff_sec"'"%60}'
+}
+
+function progress_bar {
    local i
    local varTime=$1
    local whatTime=$2
+   local desired=$(date -d"$varTime minutes" +'%s')
+   local current
    i=0
 
-   while [ $i -lt $((varTime * 2)) ]
+   while [ $i -le $((varTime * 60)) ]
    do
-      echo $((i * 50/varTime))
+      echo $((i * 1/varTime * 100/60))
       i=$((i+1))
-      sleep 30
+      current=$(date +'%s')
+      echo "# $whatTime: $(trans_sec desired current) "
+      if [ $i -eq $((varTime * 60)) ]
+      then
+         #beep -r 10 -f 1500 -l 200 -d 1000
+         #sleep 1
+         beep -f 1500 -l 2000
+      fi
+      sleep 1
    done | zenity --text="$whatTime" --progress --auto-close
-      
-   beep -r 10 -f 1500 -l 200 -d 1000
-   sleep 1
-   beep -f 1500 -l 2000
 }
 
 function open_file {
@@ -38,19 +52,16 @@ if [ ! -x /usr/bin/beep ]; then
    gksudo apt-get install beep
 fi
 
-zenity --info --text="Pomodoro Technique Counter"
+#zenity --info --text="Pomodoro Technique Counter"
 
-POMODORO_TIME=$(zenity --entry --title="Pomodoro time" --text="Enter the time in minutes" --entry-text "25")
-BREAK_TIME=$(zenity --entry --title="Break time" --text="Enter the time in minutes" --entry-text "5")
-LONG_BREAK_TIME=$(zenity --entry --title="Long Break time" --text="Enter the time in minutes" --entry-text "20")
+POMODORO_TIME=25
+BREAK_TIME=5
+LONG_BREAK_TIME=20
 
 while [ answer != "0" ]
 do
 
-answer=$(zenity --list --radiolist --width=350 --height=300 \\
-   --column "Selection" --column "Choice" --column "Description" \\
-   FALSE 0 "Exit" FALSE 1 "Start Pomodoro" FALSE 2 "Break" \\ 
-   FALSE 3 "Long Breaks" TRUE 4 "Open TodoToday List" )
+answer=$(zenity --list --radiolist --width=350 --height=300 --column "Selection" --column "Choice" --column "Description" FALSE 0 "Exit" FALSE 1 "Start Pomodoro" FALSE 2 "Break" FALSE 3 "Long Breaks" TRUE 4 "Open TodoToday List" FALSE 5 "Set Default Time"  )
 
    case $answer in
    0) break ;;
@@ -65,6 +76,11 @@ answer=$(zenity --list --radiolist --width=350 --height=300 \\
    ;;
    4)
       open_file
+   ;;
+   5)      
+      POMODORO_TIME=$(zenity --entry --title="Pomodoro time" --text="Enter the time in minutes" --entry-text "25")
+      BREAK_TIME=$(zenity --entry --title="Break time" --text="Enter the time in minutes" --entry-text "5")
+      LONG_BREAK_TIME=$(zenity --entry --title="Long Break time" --text="Enter the time in minutes" --entry-text "20")
    ;;
    *) break ;;
    esac
